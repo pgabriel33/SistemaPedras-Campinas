608c7363619b2684512fbfbc767c92853766d3d3
LuanLeiteLeao <luanleiteraio@gmail.com>
2020-03-05 16:52:52 -0300

    index,login,cadastro

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('cadastra/', views.register.as_view(),name='cadastra'),
     path('informacoes/<nome_user>/', views.novo_user,name='informacoes'),
+    path('login/',views.user_login, name='login')
    
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from .form import FormUser
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .form import LoginForm
 
 class register(generic.CreateView):
     form_class = UserCreationForm
@@ -29,4 +32,23 @@ def novo_user(request,nome_user):
 
     contexto = {'form':FormUser() }
     return render(request,'users/cadastra_usuaria.html',contexto)
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],
+                   password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('/')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid Login')
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
 
